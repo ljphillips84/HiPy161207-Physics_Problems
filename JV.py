d6c26d194cb26c77650a215ad38de2ab80fcbdf7
ljphillips84 <laurie.phillips@liverpool.ac.uk>
2016-12-05 15:55:21 +0000

    Added resistance functions

--- JV.py
+++ JV.py
@@ -1,27 +1,46 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
-def getVoc(results):
+def getVoc(data):
     modCurrents = []
-    for entry in results:
+    for entry in data:
         modCurrents.append(abs(entry[1]))
-    return(results[modCurrents.index(min(modCurrents))][0])
+    return abs(data[modCurrents.index(min(modCurrents))][0])
 
-def getJsc(results):
+def getJsc(data):
     modVoltages = []
-    for entry in results:
+    for entry in data:
         modVoltages.append(abs(entry[0]))
-    return(results[modVoltages.index(min(modVoltages))][1])
+    return abs(data[modVoltages.index(min(modVoltages))][1])
 
-def getFF(results, Voc, Jsc):
+def getFF(data):
     power = []
-    for entry in results:
+    for entry in data:
         power.append(entry[0]*entry[1])
-    return(min(power)/(Voc*Jsc))
+    return abs(min(power)/(getVoc(data)*getJsc(data)))
 
-def getEff(Voc, Jsc, FF):
-    return(Voc*Jsc*FF)
+def getEff(data):
+    return abs(getVoc(data)*getJsc(data)*getFF(data))
+
+def getRseries(dataSubset):
+#    print(dataSubset)
+    x = []
+    y = []
+    for i in range(0,len(dataSubset)):
+        x.append(dataSubset[i][0])
+        y.append(dataSubset[i][1])
+    return 1000/stats.linregress(x, y)[0], abs(stats.linregress(x, y)[1]/stats.linregress(x, y)[0])
+
+def getRshunt(dataSubset):
+#    print(dataSubset)
+    x = []
+    y = []
+    for i in range(0,len(dataSubset)):
+        x.append(dataSubset[i][0])
+        y.append(dataSubset[i][1])
+    return 1000/stats.linregress(x, y)[0], abs(stats.linregress(x, y)[1]/stats.linregress(x, y)[0])
 
 results = []
 with open('1-1.txt', newline='') as inputfile:
@@ -46,13 +65,16 @@ plt.xlabel('Voltage (V)')
 plt.ylabel('Current Density (mA cm^-2)')
 plt.title('J-V Curve')
 plt.grid(True)
-#plt.savefig("test.png")
+plt.axhline(0, linewidth="2", color="black")
+plt.axvline(0, linewidth="2", color="black")
 plt.show()
 
 
 print(results)
+print(getRseries(results[1:3][0:2]))
+print(getRseries(results[1:3][0:2]))
 print("Voc = " + str.format('{0:.2f}', getVoc(results)) + " V")
 print("Jsc = " + str.format('{0:.1f}', getJsc(results)) + " mA cm^-2")
-print("FF = " + str.format('{0:.1f}', 100*getFF(results, getVoc(results), getJsc(results))) + "%")
-print("Eff = " + str.format('{0:.1f}', getEff(getVoc(results), getJsc(results), getFF(results, getVoc(results), getJsc(results)))) + "%")
+print("FF = " + str.format('{0:.1f}', 100*getFF(results)) + "%")
+print("Eff = " + str.format('{0:.1f}', getEff(results)) + "%")
 
