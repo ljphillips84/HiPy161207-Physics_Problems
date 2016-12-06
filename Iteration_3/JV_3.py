9a9babda2b406fe549d0b60334edd97ca2bd936f
ljphillips84 <ljphillips84@users.noreply.github.com>
2016-12-06 18:24:29 +0000

    Update JV_3.py

--- Iteration_3/JV_3.py
+++ Iteration_3/JV_3.py
@@ -1,23 +1,13 @@
-import csv
+import numpy as np
 import os
-
 import matplotlib.pyplot as plt
-
-from Iteration_3 import getjvparameters as params
+import jvanalysis as jv
 
 for directory, subdirectories, files in os.walk('/home/ljp84/git-repos/HiPy161207-Physics/'):
     for file in files:
         if file[-4:] == ".txt" and file[1] == "-":
-            results = []
-            with open(file, newline='') as inputfile:
-                for row in csv.reader(inputfile):
-                    results.append(str(row[0]).split())
 
-            for i in range(0, len(results)):
-                results[i][0] = float(results[i][0])
-
-            for i in range(0, len(results)):
-                results[i][1] = 1000 * float(results[i][1]) / 0.1
+            results = jv.i_to_j(np.loadtxt("1-1.txt"))
 
             voltages = []
             currents = []
@@ -33,12 +23,12 @@ for directory, subdirectories, files in os.walk('/home/ljp84/git-repos/HiPy16120
             plt.grid(True)
             plt.axhline(0, linewidth="2", color="black")
             plt.axvline(0, linewidth="2", color="black")
-            plt.savefig(str(file[0:3])+'.png')
+            #plt.savefig(str(file[0:3])+'.png')
             #plt.show()
 
-            print("Eff = " + str.format('{0:.2f}', params.getparameters(results)[0]) + "%")
-            print("Voc = " + str.format('{0:.3f}', params.getparameters(results)[1]) + " V")
-            print("Jsc = " + str.format('{0:.2f}', params.getparameters(results)[2]) + " mA cm^-2")
-            print("FF = " + str.format('{0:.2f}', 100 * params.getparameters(results)[3]) + "%")
-            print("Rs = " + str.format('{0:.2f}', params.getparameters(results)[4]) + " Ohm cm^2")
-            print("Rsh = " + str.format('{0:.2f}', params.getparameters(results)[5]) + " Ohm cm^2")
+            print("Eff = " + str.format('{0:.2f}', jv.getparameters(results)[0]) + "%")
+            print("Voc = " + str.format('{0:.3f}', jv.getparameters(results)[1]) + " V")
+            print("Jsc = " + str.format('{0:.2f}', jv.getparameters(results)[2]) + " mA cm^-2")
+            print("FF = " + str.format('{0:.2f}', 100 * jv.getparameters(results)[3]) + "%")
+            print("Rs = " + str.format('{0:.2f}', jv.getparameters(results)[4]) + " Ohm cm^2")
+            print("Rsh = " + str.format('{0:.2f}', jv.getparameters(results)[5]) + " Ohm cm^2")
