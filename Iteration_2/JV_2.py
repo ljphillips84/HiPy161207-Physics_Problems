2ac42e8b2b723014c97f7515697f01ceaf3017b7
ljphillips84 <ljphillips84@users.noreply.github.com>
2016-12-06 18:21:56 +0000

    Update JV_2.py

--- Iteration_2/JV_2.py
+++ Iteration_2/JV_2.py
@@ -14,30 +14,25 @@ def getparameters(data):
         power.append(entry[0] * entry[1])
 
     Vocsubset = data[max(modCurrents.index(min(modCurrents))-1,0):(modCurrents.index(min(modCurrents))+2)][0:3]
-    Voc = abs(stats.linregress(Vocsubset)[1]/stats.linregress(Vocsubset)[0])# abs(data[modCurrents.index(min(modCurrents))][0])
+    Voc = abs(stats.linregress(Vocsubset)[1]/stats.linregress(Vocsubset)[0])
 
     Jscsubset = data[max(modVoltages.index(min(modVoltages)) - 1, 0):(modVoltages.index(min(modVoltages)) + 2)][0:3]
-    Jsc = abs(stats.linregress(Jscsubset)[1])# abs(data[modVoltages.index(min(modVoltages))][1])
+    Jsc = abs(stats.linregress(Jscsubset)[1])
 
     FF = abs(min(power)/(Voc*Jsc))
 
-    Rs = 1000/stats.linregress(Vocsubset)[0]
+    Rs = 1000/abs(stats.linregress(Vocsubset)[0])
 
-    Rsh = 1000/stats.linregress(Jscsubset)[0]
+    Rsh = 1000/abs(stats.linregress(Jscsubset)[0])
 
     return Voc*Jsc*FF, Voc, Jsc, FF, Rs, Rsh
 
+def i_to_j(data):
+    for i in range(0, len(data)):
+        data[i][1] = 1000 * float(data[i][1]) / 0.1
+    return data
 
-results = []
-with open('2-1.txt', newline='') as inputfile:
-    for row in csv.reader(inputfile):
-        results.append(str(row[0]).split())
-
-for i in range(0,len(results)):
-    results[i][0] = float(results[i][0])
-
-for i in range(0,len(results)):
-    results[i][1] = 1000*float(results[i][1])/0.1
+results = i_to_j(np.loadtxt("1-1.txt"))
 
 voltages = []
 currents = []
@@ -62,5 +57,3 @@ print("Jsc = " + str.format('{0:.2f}', getparameters(results)[2]) + " mA cm^-2")
 print("FF = " + str.format('{0:.2f}', 100*getparameters(results)[3]) + "%")
 print("Rs = " + str.format('{0:.2f}', getparameters(results)[4]) + " Ohm cm^-2")
 print("Rsh = " + str.format('{0:.2f}', getparameters(results)[5]) + " Ohm cm^-2")
-
-
